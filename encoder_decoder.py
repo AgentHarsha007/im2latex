ca595ddd0b79958cfc8637f76af40e2e033f88e7
AgentHarsha007 <mvvharshavardhan@gmail.com>
2025-08-24 12:41:18 +0530

    encoder_decoder added

---
+++ encoder_decoder.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+class SinusoidalPositionalEncoding(nn.Module):
+    def __init__(self, embed_dim, max_len=5000):
+        super().__init__()
+        pe = torch.zeros(max_len, embed_dim)
+        position = torch.arange(0, max_len).unsqueeze(1).float()
+        div_term = torch.exp(torch.arange(0, embed_dim, 2).float() * (-math.log(10000.0) / embed_dim))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)  # [1, max_len, embed_dim]
+        self.register_buffer('pe', pe)
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1)]
+class encoderblock(torch.nn.Module):
+    def __init__(self,num_heads, embed_size, dropout,forward_expansion):
+      super().__init__()
+      self.attention=nn.MultiheadAttention(embed_size,num_heads=num_heads)
+      self.feedforward=nn.Sequential(
+          nn.Linear(embed_size,forward_expansion*embed_size),
+          nn.ReLU(),
+          nn.Linear(forward_expansion*embed_size,embed_size)
+      )
+      self.norm1=nn.LayerNorm(embed_size)
+      self.norm2=nn.LayerNorm(embed_size)
+      self.dropout1=nn.Dropout(dropout)
+      self.dropout2=nn.Dropout(dropout)
+    def forward(self,input):
+      x=input.transpose(0,1)
+      attention_out,_=self.attention(x,x,x)
+      add_norm=self.norm1(self.dropout1(attention_out)+x)
+      x=self.feedforward(add_norm)
+      x=self.norm2(add_norm+self.dropout2(x))
+      x=x.transpose(0,1)
+      return x
+class decoderblock(torch.nn.Module):
+  def __init__(self,num_heads,embed_size,dropout,forward_expasion):
+    super().__init__()
+    self.cross_attention=nn.MultiheadAttention(embed_size,num_heads=num_heads)
+    self.attention=nn.MultiheadAttention(embed_size,num_heads=num_heads)
+    self.dropout1=nn.Dropout(dropout)
+    self.dropout2=nn.Dropout(dropout)
+    self.dropout3=nn.Dropout(dropout)
+    self.feed_forward=nn.Sequential(
+        nn.Linear(embed_size,forward_expasion*embed_size),
+        nn.ReLU(),
+        nn.Linear(forward_expasion*embed_size,embed_size)
+    )
+    self.norm1=nn.LayerNorm(embed_size)
+    self.norm2=nn.LayerNorm(embed_size)
+    self.norm3=nn.LayerNorm(embed_size)
+  def forward(self,encoder_input,decoder_input,padding_mask=None,attn_mask=None):
+    x=decoder_input.transpose(0,1)
+    attention_out,_=self.attention(x,x,x,attn_mask=attn_mask,key_padding_mask=padding_mask)
+    add_norm=self.norm1(self.dropout1(attention_out)+x)
+    cross_attention_out,_=self.cross_attention(add_norm,encoder_input.transpose(0,1),encoder_input.transpose(0,1))
+    add_norm=self.norm2(self.dropout2(cross_attention_out)+add_norm)
+    feed_out=self.feed_forward(add_norm)
+    x=self.norm3(self.dropout3(feed_out)+add_norm)
+    x=x.transpose(0,1)
+    return x
